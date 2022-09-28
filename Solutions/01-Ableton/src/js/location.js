377a1db557d75cc24862aff0ad359f1734f52435
James Alderson <james.alderson.a@gmail.com>
2022-09-28 22:24:19 +0330

    Change location api
    - Change location api from 'ip-api.com' to 'ipgeolocation.io'.

--- Solutions/01-Ableton/src/js/location.js
+++ Solutions/01-Ableton/src/js/location.js
@@ -1,11 +1,13 @@
 const LOCATION = document.querySelector("#location")
-const LOCATIONS = document.querySelectorAll("#location option")
-const CLIENT_IP = "http://ip-api.com/json/"
+const LOCATIONS = LOCATION.querySelectorAll("option")
+const LOCATION_API = "https://api.ipgeolocation.io/ipgeo"
+const API_KEY = "68c424493d544079b9621d10f36ccd73"
+const API_URL = `${LOCATION_API}?apiKey=${API_KEY}`
 
-fetch(CLIENT_IP)
+fetch(API_URL)
   .then(res => res.json())
   .then(data => {
-    let countryCode = data.countryCode.toLowerCase()
+    let countryCode = data.country_code2.toLowerCase()
 
     checkCountryCode(countryCode)
   })
